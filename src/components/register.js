f058584a5f0c9a4fd76a9a99a5281e3329ce8abc
fadiHB <eng.fadi.hb@gmail.com>
2021-02-01 13:22:59 +0000

    register page

--- src/components/register.js
+++ src/components/register.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import { Navbar, Nav  } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import './css_components/style.css';
 
 function Register() {
   return (
-    <div className = 'col-sm-6 offset-sm-3'>
+    <div >
       <div>
         <Navbar bg="dark" variant="dark">
           <Nav className="mr-auto">
@@ -13,9 +14,14 @@ function Register() {
           </Nav>
         </Navbar>
       </div>
-      <form method="POST">
-        <label>User Name</label>
-      </form>
+      <div className = 'col-sm-6 offset-sm-3'>
+        <input className='form-control'  placeholder='User Name' type='text' />
+        <input className='form-control'  placeholder='Pass Word' type='password' />
+        <input className='form-control'  placeholder='Your First Name' type='text' />
+        <input className='form-control'  placeholder='Your Last Name' type='text' />
+        <input className='form-control'  placeholder='Your Email' type='email' />
+        <button className='btn btn-primary' >Sign Up</button>
+      </div>
     </div>
   );
 }
